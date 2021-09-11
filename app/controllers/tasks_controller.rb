bac8894c9271d57e6cf48087d1b4c80db2278090
ArakawaMayumi <uudpinooo@gmail.com>
2021-09-12 02:44:15 +0900

    新規登録機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,9 +7,20 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:notice] = "タスクを登録しました"
+      redirect_to task_url(@task)
+    else
+      flash[:notice] = "タスクの登録に失敗しました"
+      render new_task_path
+    end
+    
   end
 
   def edit
@@ -19,5 +30,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
