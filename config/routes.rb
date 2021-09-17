f067c9eba6f061fd1cfd6604334d42b9e89efbbb
ArakawaMayumi <uudpinooo@gmail.com>
2021-09-17 22:45:56 +0900

    Sessions_controller生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get '/login', to: 'sessions#new'
+  root to: 'sessions#new'
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
