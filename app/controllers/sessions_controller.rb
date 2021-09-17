53d7dd363600a36bd8247fb78f419556c644c51f
ArakawaMayumi <uudpinooo@gmail.com>
2021-09-18 01:30:36 +0900

    ログイン機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to tasks_url
+      flash[:notice] = "ログインしました"
+    else
+      flash[:alert] = ["ログインできませんでした"]
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get '/login', to: 'sessions#new'
   root to: 'sessions#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
