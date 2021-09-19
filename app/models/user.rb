95643a6b422742abaf5fe123c1779607c348d244
ArakawaMayumi <uudpinooo@gmail.com>
2021-09-19 23:18:43 +0900

    Userモデルバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true, length: {maximum: 20}
+  validates :email, presence: true, uniqueness: true, length: {maximum: 256}
+
 end
