be20dfda81218b52091b8922d485c85251dcd1fb
ArakawaMayumi <uudpinooo@gmail.com>
2021-10-10 22:47:21 +0900

    spec letを使った共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,26 +2,37 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧機能表示' do
+    # letは遅延評価のため呼び出されるまでは実行されない
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+
     before do
-      # ユーザーAを作成
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
       # 作成者がユーザーAであるタスクを作成
       FactoryBot.create(:task, name: 'ユーザーAのタスク1', description: 'ユーザーAが作成したタスクの1つ目です', user: user_a)
+
+      # ユーザーAでログインする
+      # Capybara使用
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログイン'
     end
 
     context 'ユーザーAがログインしているとき' do
-      before do
-        # ユーザーAでログインする
-        # Capybara使用
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログイン'
-      end
+      let(:login_user) { user_a }
 
       it 'ユーザーAが作成したタスクが表示される' do
         # 作成済みタスクの名称が画面上に表示されていることを確認
         expect(page).to have_content 'ユーザーAのタスク1'
+      end
+    end
+
+    context 'ユーザーBがログインしているとき' do
+      let(:login_user) { user_b }
+
+      it 'ユーザーAが作成したタスクは表示されない' do
+        #ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
+        expect(page).not_to have_content 'ユーザーAのタスク1'
       end
     end
   end
